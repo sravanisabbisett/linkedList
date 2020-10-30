d7715648e4a688abe2cc20d80b2e4a933f94bd8a
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-10-30 07:34:30 +0530

    ADDED:add method to add the data at the top

--- src/main/java/linkedList/MyLinkedList.java
+++ src/main/java/linkedList/MyLinkedList.java
@@ -9,4 +9,28 @@ public class MyLinkedList {
         this.tail=null;
     }
 
+    public void add(INode newNode) {
+        if(this.tail==null) {
+            tail = newNode;
+        }
+        if(this.head==null){
+            this.head=newNode;
+        }else {
+            INode tempNode=this.head;
+            this.head=newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+    public void PrintMyNodes(){
+        StringBuffer myNodes=new StringBuffer("MyNodes:");
+        INode tempNode=head;
+        while (tempNode.getNext()!=null){
+            myNodes.append(tempNode.getKey());
+            if(tempNode.equals(tail)) myNodes.append("->");
+            tempNode=tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
+
 }
--- src/test/java/com/Testing/LinkedListTest.java
+++ src/test/java/com/Testing/LinkedListTest.java
@@ -18,5 +18,20 @@ public class LinkedListTest {
                 MySecondNode.getNext().equals(MyThirdNode);
         Assert.assertTrue(result);
     }
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddAtTop() {
+        MyNode<Integer> MyFirstNode=new MyNode<>(70);
+        MyNode<Integer> MySecondNode=new MyNode<>(30);
+        MyNode<Integer> MyThirdNode=new MyNode<>(56);
+        MyLinkedList linkedList=new MyLinkedList();
+        linkedList.add(MyFirstNode);
+        linkedList.add(MySecondNode);
+        linkedList.add(MyThirdNode);
+        boolean result=MyLinkedList.head.equals(MyThirdNode)&&
+                MyLinkedList.head.getNext().equals(MySecondNode)&&
+                MyLinkedList.tail.equals(MyFirstNode);
+        Assert.assertTrue(result);
+        linkedList.PrintMyNodes();
+    }
 
 }
