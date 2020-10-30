4b1781c659d5876ab800e9b69e483623dcb4c8e4
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-10-30 20:13:24 +0530

    ADDED:dequeue method to empty the queue

--- src/main/java/linkedList/MyQueue.java
+++ src/main/java/linkedList/MyQueue.java
@@ -25,4 +25,8 @@ public class MyQueue {
     public boolean isEmpty(){
         return myStack.IsEmpty();
     }
+
+    public void deQueue() {
+        myStack.emptyingTheStack();
+    }
 }
--- src/test/java/com/Testing/QueueTest.java
+++ src/test/java/com/Testing/QueueTest.java
@@ -20,5 +20,18 @@ public class QueueTest {
         Assert.assertEquals(myFirstNode,result);
 
     }
+    @Test
+    public void givenThreeNumbersWhenDequeueFromBeginingShouldEmptyTheDequeue(){
+        MyNode<Integer> myFirstNode=new MyNode<>(56);
+        MyNode<Integer> mySecondNode=new MyNode<>(30);
+        MyNode<Integer> myThirdNode=new MyNode<>(70);
+        MyQueue queue=new MyQueue();
+        queue.enqueue(myFirstNode);
+        queue.enqueue(mySecondNode);
+        queue.enqueue(myThirdNode);
+        queue.deQueue();
+        boolean result=queue.isEmpty();
+        Assert.assertTrue(result);
+    }
 
 }
