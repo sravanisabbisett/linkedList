56a22b2a44be20ca92d9d9dd92861a3432f4e6e0
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-01 20:02:21 +0530

    ADDED:add and size method to get the size of the binary tree

--- src/test/java/com/Testing/BinaryNodeTest.java
+++ src/test/java/com/Testing/BinaryNodeTest.java
@@ -14,5 +14,23 @@ public class BinaryNodeTest {
         int size=binaryTree.getSize();
         Assert.assertEquals(3,size);
     }
-
+    Test
+    public void given13_NumbersWhenaddedToTheBinaryTree_ShouldReturnSize13(){
+        BinaryTree<Integer> binaryTree=new BinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(65);
+        binaryTree.add(63);
+        binaryTree.add(67);
+        int size=binaryTree.getSize();
+        Assert.assertEquals(13,size);
+    }
 }
